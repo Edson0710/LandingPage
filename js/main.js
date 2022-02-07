c8d4dd00f73b8adc649945dafaf2e164eb0c3a2e
Edson0710 <edsonnavarro0710@gmail.com>
2022-02-06 21:17:17 -0600

    skills

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
-var typed = new Typed('.typed', {
-    strings: [
-        "Hello World!",
-        "I'm Edson Navarro.",
-        "I'm a Mexican Developer.",
-        "I'm a Full Stack Developer.",
-    ],
-    typeSpeed: 50,
-    backSpeed: 20,
-    backDelay: 1500,
-    startDelay: 500,
-    loop: true,
+$(document).ready(function () {
+
+    var typed = new Typed('.typed', {
+        strings: [
+            "Hello World! 🌎",
+            "I'm Edson Navarro.",
+            "I'm a Mexican Developer. 🌮",
+            "I'm a Full Stack Developer. 💻",
+        ],
+        typeSpeed: 50,
+        backSpeed: 20,
+        backDelay: 1500,
+        startDelay: 500,
+        loop: true,
+        contentType: 'html',
+
+    });
+
+    console.log("Document ready!");
+    $(".skills").addClass("active")
+    $(".skills .skill .skill-bar span").each(function() {
+    $(this).animate({
+        "width": $(this).parent().attr("data-bar") + "%"
+    }, 1000);
+    $(this).append('<b>' + $(this).parent().attr("data-bar") + '%</b>');
+    });
+    setTimeout(function() {
+    $(".skills .skill .skill-bar span b").animate({"opacity":"1"},1000);
+    }, 2000);
 });
