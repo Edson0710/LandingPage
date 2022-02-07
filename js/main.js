9349fc014be289e8ea1a129a017e602a9aca053c
Edson0710 <edsonnavarro0710@gmail.com>
2022-02-06 19:32:38 -0600

    Banner principal

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var typed = new Typed('.typed', {
+    strings: [
+        "Hello World!",
+        "I'm Edson Navarro.",
+        "I'm a Mexican Developer.",
+        "I'm a Full Stack Developer.",
+    ],
+    typeSpeed: 50,
+    backSpeed: 20,
+    backDelay: 1500,
+    startDelay: 500,
+    loop: true,
+});
